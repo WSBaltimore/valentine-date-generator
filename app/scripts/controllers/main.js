6da908879f56eed20a5167d30a28ecf8ce068975
Josh Lawrence <jwlawrence@gmail.com>
2014-01-15 21:35:23 -0500

    auth working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,51 +1,72 @@
 'use strict';
 
-app.controller('MainCtrl', function ($scope, $http, $firebaseAuth) {
+app.factory('firebaseAuth', function ($rootScope) {
+	var auth = {};
 	var firebase = new Firebase('https://valentine-date-generator.firebaseio.com');
-
-	// AngularFire approach
-	$scope.auth = $firebaseAuth(firebase);
-
-	$scope.$on("$firebaseAuth:login", function(e, user) {
-		$scope.auth.user = user;
-		console.log($scope.auth.user);
-
-		// Get friend info
-		$http({
-			method: 'GET',
-			url: 'https://graph.facebook.com/'+ $scope.auth.user.thirdPartyUserData.id +'?access_token='+ $scope.auth.user.accessToken +'&fields=id,name,gender,age_range,location,relationship_status,significant_other,friends.fields(name,age_range,gender,location,relationship_status,significant_other)'
-		}).success(function(data, status, headers, config) {
-			console.log(data);
-			$scope.fbData = data;;
-		}).error(function(data, status, headers, config) {
-			console.log(data);
-		});
+	var client = new FirebaseSimpleLogin(firebase, function(error, user) {
+		if (error) {
+			console.log(error);
+		} else if (user) {
+			auth.user = user;
+			auth.broadcastAuthEvent();
+		} else {
+			auth.user = null;
+			auth.broadcastAuthEvent();
+		}
 	});
 
-	// Firebase approach
-	// var auth = new FirebaseSimpleLogin(firebase, function(error, user) {
-	// 	if (error) {
-	// 		console.log(error);
-	// 	} else if (user) {
-	// 		// user authenticated with Firebase
-	// 		console.log(user);
-	// 	} else {
-	// 		// user is logged out
-	// 		console.log('logged out');
-	// 	}
-	// });
+	var broadcastAuthEvent = function() {
+		$rootScope.$broadcast('authEvent');
+	};
 
-	// console.log(auth);
+	var login = function() {
+		client.login('facebook', {
+			rememberMe: true,
+			scope: 'email,user_likes,user_birthday,user_checkins,user_hometown,user_interests,user_location,user_photos,user_relationships,user_relationship_details,friends_likes,friends_birthday,friends_checkins,friends_hometown,friends_interests,friends_location,friends_photos,friends_relationships,friends_relationship_details'
+		});
+	};
 
-	// $scope.login = function() {
-	// 	auth.login('facebook', {
-	// 		rememberMe: true,
- //  			scope: 'email,user_likes,user_birthday,user_checkins,user_hometown,user_interests,user_location,user_photos,user_relationships,user_relationship_details,friends_likes,friends_birthday,friends_checkins,friends_hometown,friends_interests,friends_location,friends_photos,friends_relationships,friends_relationship_details'
-	// 	});
-	// };
+	var logout = function() {
+		client.logout();
+	};
 
-	// $scope.logout = function() {
-	// 	auth.logout();
-	// };
+	return auth = {
+		broadcastAuthEvent: broadcastAuthEvent,
+		login: login,
+		logout: logout
+	}
+});
+
+app.controller('MainCtrl', function ($scope, firebaseAuth) {
+
+	$scope.login = function() {
+		firebaseAuth.login();
+	};
+
+	$scope.logout = function() {
+		firebaseAuth.logout();
+	};
+
+	$scope.isLoggedIn = function() {
+		return !!$scope.user;
+	};
+
+	// src: Alex Vanston (https://coderwall.com/p/ngisma)
+	$scope.safeApply = function(fn) {
+		var phase = this.$root.$$phase;
+		if (phase == '$apply' || phase == '$digest') {
+			if (fn && (typeof(fn) === 'function')) {
+				fn();
+			}
+		} else {
+			this.$apply(fn);
+		}
+	};
+
+	$scope.$on('authEvent', function() {
+		$scope.safeApply(function() {
+			$scope.user = firebaseAuth.user;
+		});
+	});
 
 });
