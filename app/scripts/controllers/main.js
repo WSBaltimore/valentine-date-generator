f899bb31b36c2ea1d6248a82474a07709123b3d7
Josh Lawrence <jlawrence@webershandwick.com>
2014-01-17 17:35:47 -0500

    promise-ized

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,54 +1,50 @@
 'use strict';
 
-app.factory('firebaseAuth', function ($rootScope, $http) {
-	var auth = {};
+app.factory('firebaseAuth', function ($http, $q) {
 	var firebase = new Firebase('https://valentine-date-generator.firebaseio.com');
+	var deferredData = $q.defer();
 
-	auth.client = new FirebaseSimpleLogin(firebase, function(error, user) {
+	new FirebaseSimpleLogin(firebase, function(error, user) {
 		if (error) {
-			console.log(error);
+			deferredData.reject(error);
 		} else if (user) {
-			auth.user = user;
-			$rootScope.$broadcast('authEvent');
+			deferredData.resolve(user);
 		}
 	});
 
-	auth.login = function() {
-		auth.client.login('facebook', {
-			rememberMe: true,
-			scope: 'email,user_likes,user_birthday,user_checkins,user_hometown,user_interests,user_location,user_photos,user_relationships,user_relationship_details,friends_likes,friends_birthday,friends_checkins,friends_hometown,friends_interests,friends_location,friends_photos,friends_relationships,friends_relationship_details'
-		});
-	};
+	return {
+		user: function () {
+			return deferredData.promise;
+		},
 
-	auth.logout = function() {
-		auth.client.logout();
-	};
+		login: function() {
+			simpleLogin.login('facebook', {
+				rememberMe: true,
+				scope: 'email,user_likes,user_birthday,user_checkins,user_hometown,user_interests,user_location,user_photos,user_relationships,user_relationship_details,friends_likes,friends_birthday,friends_checkins,friends_hometown,friends_interests,friends_location,friends_photos,friends_relationships,friends_relationship_details'
+			});
+		},
 
-	auth.getFacebookData = function (userId, accessToken) {
-		return $http.get('https://graph.facebook.com/' + userId + '?access_token=' + accessToken + '&fields=id,name,age_range,relationship_status,birthday,education,gender,interested_in,hometown,location,significant_other,security_settings,checkins,family,friends.fields(name,age_range,birthday,relationship_status,gender,hometown,interested_in,significant_other,security_settings,email,location),mutualfriends,picture,email');
-	};
+		logout: function() {
+			simpleLogin.logout();
+		},
 
-	return auth;
+		getFacebookData: function(userId, accessToken) {
+			return $http.get('https://graph.facebook.com/' + userId + '?access_token=' + accessToken + '&fields=id,name,age_range,relationship_status,birthday,education,gender,interested_in,hometown,location,significant_other,security_settings,checkins,family,friends.fields(name,age_range,birthday,relationship_status,gender,hometown,interested_in,significant_other,security_settings,email,location),mutualfriends,picture,email');
+		}
+	}
+
 });
 
 app.controller('MainCtrl', function ($scope, firebaseAuth) {
+
 	var auth = firebaseAuth;
 
-	$scope.login = auth.login;
-	$scope.logout = auth.logout;
-
-	// Once the "user" object is returned from logging in...
-	$scope.$on('authEvent', function() {
-		$scope.safeApply(function() {
-			$scope.user = auth.user;
-
-			// Get data from Facebook
-			auth.getFacebookData($scope.user.id, $scope.user.accessToken).then(function (data) {
-				var fbData = data.data;
-				$scope.friends = fbData.friends.data;
-				console.log($scope.friends);
-			});
-
-		});
+	auth.user().then(function (data) {
+		$scope.user = data;
+		console.log($scope.user);
+	},
+	function (error) {
+		console.log(error);
 	});
+
 });
