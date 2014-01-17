3a874d1f5a2834434a2d63df0a31c3f7a30a81a2
Josh Lawrence <jwlawrence@gmail.com>
2014-01-17 00:41:35 -0500

    clean up some code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,53 +3,41 @@
 app.factory('firebaseAuth', function ($rootScope) {
 	var auth = {};
 	var firebase = new Firebase('https://valentine-date-generator.firebaseio.com');
-	var client = new FirebaseSimpleLogin(firebase, function(error, user) {
-		if (error) {
-			console.log(error);
-		} else if (user) {
-			auth.user = user;
-			auth.broadcastAuthEvent();
-		} else {
-			auth.user = null;
-			auth.broadcastAuthEvent();
-		}
-	});
-
 	var broadcastAuthEvent = function() {
 		$rootScope.$broadcast('authEvent');
 	};
 
-	var login = function() {
-		client.login('facebook', {
+	auth.client = new FirebaseSimpleLogin(firebase, function(error, user) {
+		if (error) {
+			console.log(error);
+		} else if (user) {
+			auth.user = user;
+			broadcastAuthEvent();
+		} else {
+			auth.user = null;
+			broadcastAuthEvent();
+		}
+	});
+
+	auth.login = function() {
+		auth.client.login('facebook', {
 			rememberMe: true,
 			scope: 'email,user_likes,user_birthday,user_checkins,user_hometown,user_interests,user_location,user_photos,user_relationships,user_relationship_details,friends_likes,friends_birthday,friends_checkins,friends_hometown,friends_interests,friends_location,friends_photos,friends_relationships,friends_relationship_details'
 		});
 	};
 
-	var logout = function() {
-		client.logout();
+	auth.logout = function() {
+		auth.client.logout();
 	};
 
-	return auth = {
-		broadcastAuthEvent: broadcastAuthEvent,
-		login: login,
-		logout: logout
-	}
+	return auth;
 });
 
 app.controller('MainCtrl', function ($scope, firebaseAuth) {
 
-	$scope.login = function() {
-		firebaseAuth.login();
-	};
-
-	$scope.logout = function() {
-		firebaseAuth.logout();
-	};
-
-	$scope.isLoggedIn = function() {
-		return !!$scope.user;
-	};
+	$scope.login = firebaseAuth.login;
+	$scope.logout = firebaseAuth.logout;
+	$scope.isLoggedIn = !!$scope.user;
 
 	// src: Alex Vanston (https://coderwall.com/p/ngisma)
 	$scope.safeApply = function(fn) {
@@ -64,7 +52,7 @@ app.controller('MainCtrl', function ($scope, firebaseAuth) {
 	};
 
 	$scope.$on('authEvent', function() {
-		$scope.safeApply(function() {
+		$scope.$apply(function() {
 			$scope.user = firebaseAuth.user;
 		});
 	});
