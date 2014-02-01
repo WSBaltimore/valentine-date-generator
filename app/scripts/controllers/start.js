89404acc9cbc21b941f70ee5dac4dc1b60e1c684
Josh Lawrence <jlawrence@webershandwick.com>
2014-02-01 18:39:47 -0500

    cleaned start

--- app/scripts/controllers/start.js
+++ app/scripts/controllers/start.js
@@ -1,13 +1,6 @@
 'use strict';
 
 app.controller('StartCtrl', function ($rootScope, $scope, $http, $location, firebaseAuth, activity, restaurant, gift) {
-	$scope.firebaseAuth = firebaseAuth;
-	//$scope.user = userData;
-	//$scope.friends = userData.facebook.friends.data;
-
-	// console.log($scope.user);
-	// console.log($scope.friends);
-
 
 	// Return to homepage on logout
 	$rootScope.$on("$firebaseSimpleLogin:logout", function(e, user) {
