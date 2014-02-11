a7cc4b226c37bb6541d37aa2f9a4d1017ff52e83
Steve Morton <taeo920@gmail.com>
2014-02-11 16:42:35 -0500

    added image to facebook share

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -6,13 +6,12 @@ app.controller('ResultsCtrl', function ($scope, $location, date, theDate) {
 	$scope.prefs = date.userPreferences;
 
 	$scope.share = {
-		title: "Weber Shandwick Valentine Date Generator",
-		short_description: "Need date ideas for Valentine's day? Let Weber Shandwick choose the perfect date for you.",
-		long_description: 'Weber Shandwick planned the perfect Valentines date for me with ' + theDate.partner.name + '! First I\'ll be taking ' + theDate.partner.first_name  + ' to ' + theDate.activity.name + '. Afterwards we\'ll be going to ' + theDate.restaurant.name + ' for some delicious food. I\'m going to surprise ' + theDate.pronoun + ' with ' + theDate.gift + '! I can\'t wait! Let Weber Shandwick plan a date for you too!',
+		short_title: "Weber Shandwick Valentine Date Generator",
+		long_title: 'Weber Shandwick planned the perfect Valentines date for me with ' + theDate.partner.name + '! First I\'ll be taking ' + theDate.partner.first_name  + ' to ' + theDate.activity.name + '. Afterwards we\'ll be going to ' + theDate.restaurant.name + ' for some delicious food. I\'m going to surprise ' + theDate.pronoun + ' with ' + theDate.gift + '!',
+		description: "Need date ideas for Valentine's day? Let Weber Shandwick choose the perfect date for you.",
 		link: "http://myvalentinedate.com",
 		caption: "myvalentinedate.com",
-		image: "/images/share.png",
-		hashtags: "myvalentinedate",
-		tag: theDate.partner.id
+		image: "http://myvalentinedate.com/images/share.png",
+		hashtags: "myvalentinedate"
 	};
 });
